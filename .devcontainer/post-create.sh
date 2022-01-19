53d1270a4c9cdd40494e7701d63c134548fb0413
Jerome Eteve <jerome@eteve.net>
2022-01-19 14:34:49 +0000

    optional cargo-release

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,2 +1,7 @@
 set -xe
+{% if with_cargo_release %}
 cargo install cargo-release
+{% else %}
+# Uncomment this to install cargo-release in the dev container.
+# cargo install cargo-release
+{% endif %}
