ba93a7bd7a78b01cfd9b094ce11a7e6ad0321d34
Jerome Eteve <jerome@eteve.net>
2022-04-11 20:19:49 +0100

    Added official web assembly tutorial deps

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,3 +5,12 @@ cargo install cargo-release
 # Uncomment this to install cargo-release in the dev container.
 # cargo install cargo-release
 {% endif %}
+
+{% if with_web_assembly %}
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm install --lts
+npm install npm@latest -g
+{% endif %}
